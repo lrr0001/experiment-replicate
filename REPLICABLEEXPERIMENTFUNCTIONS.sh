7af701b6f8a5dccbbf60b0d4c92ca67b40ba5c38
LRR <lrichert3@gatech.edu>
2019-03-04 00:26:15 -0500

    Removed line of code that was previously added for debuging purposes.

--- REPLICABLEEXPERIMENTFUNCTIONS.sh
+++ REPLICABLEEXPERIMENTFUNCTIONS.sh
@@ -135,7 +135,6 @@ function get_destination_info()
 
 function write_destination_info()
 {
-    echo "${REPLICABLEDIRECTORY}"
     if [ "${REPLICABLEDIRECTORY: -1}" = "/" ]
     then
         export DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}${SHA1}"
