13c2740554669549edca65e46ce60c70bf5d6842
LRR <lrichert3@gatech.edu>
2019-03-04 13:31:00 -0500

    Made a lot of changes including some bug fixes, but mostly reorganizing code to reduce replicated code and improve readability. Also, changed name of gracefully_exit_failed_replicable_experiment_script() to gracefully_exit_with_lock(). Code now informs user whenever write privileges are changed on destination directory.

--- REPLICABLEEXPERIMENTFUNCTIONS.sh
+++ REPLICABLEEXPERIMENTFUNCTIONS.sh
@@ -1,94 +1,68 @@
 #!/bin/bash
 
+function lock_destination_directory()
+{
+    if [ ! -z "${DESTINATIONDIRECTORY}" ]
+    then
+        if [ -d "${DESTINATIONDIRECTORY}" ]
+        then
+            chmod -R +rx "${DESTINATIONDIRECTORY}"
+        else
+            echo "Error: failed to lock destination directory because directory could not be found."
+        fi
+    else
+        echo "Error: failed to lock destination directory because BASH variable is null."
+    fi
+    echo "Destination directory is locked."
+}
 
+function enter_debug_mode()
+{
+    echo "Current mode: Debug"
+    echo "Debugging Mode" > "MODE.txt"
+    if [ ! -z "${DEBUGDIRECTORY}" ] && [ "" != "${DEBUGDIRECTORY}" ]
+    then
+       export "${DEBUGDIRECTORY}" > "DESTINATION.txt"
+    fi
+}
 
-
-function gracefully_exit_setup()
+function gracefully_exit_with_lock()
 {
     cd "${CURRENTDIRECTORY}"
-    if [ "$1" -eq "0" ]
-    then
-        :
-    else
-        if [ ! -z "${REPLICABLE}" ] && [ "${REPLICABLE}" -eq "0" ]
-        then
-            export REPLICABLE=1
-            echo "Current mode: Debug"
-            echo "Debugging Mode" > "MODE.txt"
-            if [ ! -z "${REVERTDESTINATIONINFO}" ] && [ "${REVERTDESTINATIONINFO}" -eq "0" ]
-            then
-            
-                if echo "${DEBUGDIRECTORY}" > "DESTINATION.txt"
-                then
-                    export REVERTDESTINATIONINFO=1
-                fi
-            fi
-
-        fi
-    fi
-    if [ ! -z "${DESTINATIONDIRECTORY}" ]
-    then
-        if [ -d "${DESTINATIONDIRECTORY}" ]
-        then
-            chmod -R +rx "${DESTINATIONDIRECTORY}"
-        fi
-    fi
-
-    exit 1
-}
-function gracefully_exit_failed_replicable_experiment_script()
-{
     export REPLICABLE=1
-    echo "Current mode: DEBUG"
-    echo "Debugging Mode" > "MODE.txt"
-        
-    if echo "${DEBUGDIRECTORY}" > "DESTINATION.txt"
-    then
-        export REVERTDESTINATIONINFO=1
-    fi
-    
-    if [ ! -z "${DESTINATIONDIRECTORY}" ]
-    then
-        if [ -d "${DESTINATIONDIRECTORY}" ]
-        then
-            chmod -R +rx "${DESTINATIONDIRECTORY}"
-        fi
-    fi
+    enter_debug_mode
+    lock_destination_directory
 
     exit 1
 }
-
-
 
 function graceful_exit()
 {
-    if [ "$1" -eq "0" ]
-    then
-        gracefully_exit_setup
-    else
-        gracefully_exit_failed_replicable_experiment_script
-    fi
+    cd "${CURRENTDIRECTORY}"
+    export REPLICABLE=1
+    enter_debug_mode
+    exit 1
 }
 
 function exit_if_directory_not_clean()
 {
-    if [ "$#" -gt 1 ]
+    if [ "$#" -gt 0 ]
     then
-        if [ -d "$2" ]
+        if [ -d "$1" ]
         then
-            cd "$2"
+            cd "$1"
         else
-            echo "Error: failed to change directory to $2."
-            graceful_exit $1
+            echo "Error: failed to change directory to $1."
+            graceful_exit
         fi
     fi
+
     if local TEMPVAR="$(git status --porcelain)"
     then
         :
     else
         echo "Error: git status command failed in $(pwd)!"
-        
-        graceful_exit $1
+        graceful_exit
     fi
     if [ -z "${TEMPVAR}" ]
     then
@@ -97,35 +71,46 @@ function exit_if_directory_not_clean()
         echo "Error: directory is not clean. Use git status command for details."
         echo "Directory: $(pwd)"
         
-        graceful_exit $1
+        graceful_exit
     fi
     cd "${CURRENTDIRECTORY}"
 }
 
+function clean_repositories_check()
+{
+    read REPLICABLEEXPERIMENTDIRECTORY < "REPLICABLE-EXPERIMENT.txt"
+    if [ -z "${REPLICABLEEXPERIMENTDIRECTORY}" ] || [ "" = "${REPLICABLEEXPERIMENTDIRECTORY}" ]
+    then
+        echo "Unable to read REPLICABLE-EXPERIMENT.txt."
+        graceful_exit
+    fi
+    exit_if_directory_not_clean "${REPLICABLEEXPERIMENTDIRECTORY}"
+    exit_if_directory_not_clean "${CURRENTDIRECTORY}"
+}
+
 function get_destination_info()
 {
-    # Additional use of variables could consolidate PATHS.txt file requirements (currently spread across get_destination_info() and write_destination_info() )
     while :
     do
         read TEMPVAR
         if [ "${TEMPVAR}" != "Debug directory:" ]
         then
             echo "First line of PATHS.txt should be Debug directory:"
-            graceful_exit $1
+            graceful_exit
         fi
         read DEBUGDIRECTORY
         read TEMPVAR
         if [ "${TEMPVAR}" != "Penultimate directory:" ]
         then
             echo "Third line of PATHS.txt should be Penultimate directory:"
-            graceful_exit $1
+            graceful_exit 
         fi
         read REPLICABLEDIRECTORY
         read TEMPVAR
         if [ "${TEMPVAR}" != "SHA1:" ]
         then
             echo "Fifth line of PATHS.txt should be SHA1:"
-            graceful_exit $1
+            graceful_exit
         fi
         read SHA1HASH
         break
@@ -135,14 +120,27 @@ function get_destination_info()
     export SHA1HASH
 }
 
-function write_destination_info()
+function build_destination_string()
 {
     if [ "${REPLICABLEDIRECTORY: -1}" = "/" ]
     then
-        export DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}${SHA1HASH}"
+        export DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}${SHA1}"
     else
-        export DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}/${SHA1HASH}"
+        export DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}/${SHA1}"
     fi
+}
+
+function write_destination_info()
+{
+    echo "Debug directory:" > "PATHS.txt"
+    echo "${DEBUGDIRECTORY}" >> "PATHS.txt"
+    echo "Penultimate directory:" >> "PATHS.txt"
+    echo "${REPLICABLEDIRECTORY}" >> "PATHS.txt"
+    echo "SHA1:" >> "PATHS.txt"
+    SHA1="$(git rev-parse --short HEAD)"
+    echo "${SHA1}" >> "PATHS.txt"
+
+    build_destination_string
 
     if [ ! -d "${DESTINATIONDIRECTORY}" ]
     then
@@ -151,92 +149,81 @@ function write_destination_info()
             :
         else
             echo "Creation of directory with path ${DESTINATIONDIRECTORY} failed!"
-            gracefully_exit_setup
+            graceful_exit
         fi
     fi
-    chmod -R +rwx "${DESTINATIONDIRECTORY}"
-    export REVERTDESTINATIONINFO=0
-    echo "Debug directory:" > "PATHS.txt"
-    echo "${DEBUGDIRECTORY}" >> "PATHS.txt"
-    echo "Penultimate directory:" >> "PATHS.txt"
-    echo "${REPLICABLEDIRECTORY}" >> "PATHS.txt"
-    echo "SHA1:" >> "PATHS.txt"
-    SHA1="$(git rev-parse --short HEAD)"
-    echo "${SHA1}" >> "PATHS.txt"
+
     echo "${DESTINATIONDIRECTORY}" > "DESTINATION.txt"
+    export DESTINATIONDIRECTORY
 }
 
+function write_to_diary()
+{
+    cd "${REPLICABLEEXPERIMENTDIRECTORY}"
+    REPLICABLEEXPERIMENTSHA1="$(git rev-parse --short HEAD)"
+    cd "${CURRENTDIRECTORY}"
+    chmod -R +rwx "${DESTINATIONDIRECTORY}" # graceful exits after this command should include lock
+    echo "$1 ${REPLICABLEEXPERIMENTSHA1}" >> "${DESTINATIONDIRECTORY}/DIARY.txt"
+    echo "Destination directory is unlocked."
+}
 
 function setup_replicable_experiment()
 {
     CURRENTDIRECTORY=$(pwd)
-    read REPLICABLEEXPERIMENTDIRECTORY < "REPLICABLE-EXPERIMENT.txt"
-    if [ -z ${REPLICABLEEXPERIMENTDIRECTORY} ] || [ "" = ${REPLICABLEEXPERIMENTDIRECTORY}} ]
-    then
-        echo "Unable to read REPLICABLE-EXPERIMENT.txt."
-        gracefully_exit_setup
-    fi
-    exit_if_directory_not_clean "0" "${REPLICABLEEXPERIMENTDIRECTORY}"
-    exit_if_directory_not_clean "0" "${CURRENTDIRECTORY}"
-    get_destination_info 1
+    clean_repositories_check
+    get_destination_info
     REPLICABLE=0
     echo "Current mode: Replicable"
     echo "Replicable Mode" > "MODE.txt"
-    write_destination_info 1
-    cd "${REPLICABLEEXPERIMENTDIRECTORY}"
-    REPLICABLEEXPERIMENTSHA1="$(git rev-parse --short HEAD)"
-    cd "${CURRENTDIRECTORY}"
-    echo "$1 ${REPLICABLEEXPERIMENTSHA1}" >> "${DESTINATIONDIRECTORY}/DIARY.txt"
-    chmod -R +rx "${DESTINATIONDIRECTORY}"
+    write_destination_info 
+    write_to_diary "$1" # graceful exits after this command should include lock.
+    lock_destination_directory
     return 0
 }
 
 
 
-
 function setup_replicable_experiment_script()
 {
+    # Check whether in DEBUG mode or REPLICABLE mode.
     read REPLICABLESTR < "MODE.txt"
     if [ -z "${REPLICABLESTR}" ] || [ "${REPLICABLESTR}" != "Replicable Mode" ]
     then
         echo "Executing $1 in DEBUG mode."
         return 0
     fi
+
     export REPLICABLE=0
     CURRENTDIRECTORY=$(pwd)
-    read REPLICABLEEXPERIMENTDIRECTORY < "REPLICABLE-EXPERIMENT.txt"
-    if [ -z "${REPLICABLEEXPERIMENTDIRECTORY}" ] || [ "" = "${REPLICABLEEXPERIMENTDIRECTORY}" ]
-    then
-        echo "Unable to read REPLICABLE-EXPERIMENT.txt."
-        graceful_exit 1
-    fi
-    exit_if_directory_not_clean "1" "${REPLICABLEEXPERIMENTDIRECTORY}"
-    exit_if_directory_not_clean "1" "${CURRENTDIRECTORY}"
+    clean_repositories_check
     get_destination_info
-    if [ "${REPLICABLEDIRECTORY: -1}" = "/" ]
+
+
+    # Check that PATHS.txt SHA1 matches current SHA1 hash:
+    SHA1="$(git rev-parse --short HEAD)"
+    if [ "${SHA1}" != "${SHA1HASH}" ]
     then
-        DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}${SHA1HASH}"
-    else
-        DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}/${SHA1HASH}"
+        echo "Error: SHA1HASH in PATHS.txt does not match current SHA1 commit ID."
+        graceful_exit
     fi
+
+    # Verify destination directory from PATHS.txt and DESTINATION.txt are the same.
+    build_destination_string
     read DESTINATIONFROMFILE < "DESTINATION.txt"
     if [ -z "${DESTINATIONFROMFILE}" ] || [ "" = "${DESTINATIONFROMFILE}" ]
     then
        echo "Error: unable to read DESTINATION.txt."
-       graceful_exit 1
+       graceful_exit
     fi
-
     if [ ! "${DESTINATIONDIRECTORY}/" = "${DESTINATIONFROMFILE}" ] && [ ! "${DESTINATIONDIRECTORY}" = "${DESTINATIONFROMFILE}" ] && [ ! "${DESTINATIONDIRECTORY}" = "${DESTINATIONFROMFILE}/" ]
     then
         echo "Error: destinations from DESTINATION.txt and PATHS.txt do not match."
         graceful_exit 1
     fi
-    cd "${REPLICABLEEXPERIMENTDIRECTORY}"
-    REPLICABLEEXPERIMENTSHA1="$(git rev-parse --short HEAD)"
-    cd "${CURRENTDIRECTORY}"
-    chmod -R +rwx "${DESTINATIONDIRECTORY}"
-    echo "$1 ${REPLICABLEEXPERIMENTSHA1}" >> "${DESTINATIONDIRECTORY}/DIARY.txt"
+
+    write_to_diary "$1"
     export DESTINATIONDIRECTORY
+    echo "Executing $1 in REPLICABLE mode."
     return 0
 }
 
@@ -244,38 +231,26 @@ function gracefully_exit_successful_replicable_experiment_script()
 {
    if [ ! -z "${REPLICABLE}" ] && [ "${REPLICABLE}" -eq "0" ]
    then
-       chmod -R +rx "${DESTINATIONDIRECTORY}"
+       lock_destination_directory
    fi
    exit 0
 }
 
 function replicable_experiment_cleanup()
 {
-    export REPLICABLE=1
-    echo "Debugging Mode" > "MODE.txt"
-    CURRENTDIRECTORY=$(pwd)
+    CURRENTDIRECTORY="$(pwd)"
+    get_destination_info
+    enter_debug_mode
+    build_destination_string
+
     read REPLICABLEEXPERIMENTDIRECTORY < "REPLICABLE-EXPERIMENT.txt"
     if [ -z "${REPLICABLEEXPERIMENTDIRECTORY}" ] || [ "" = "${REPLICABLEEXPERIMENTDIRECTORY}" ]
     then
         echo "Unable to read REPLICABLE-EXPERIMENT.txt."
-        graceful_exit 1
+        graceful_exit
     fi
-    get_destination_info 1
-    if [ "${REPLICABLEDIRECTORY: -1}" = "/" ]
-    then
-        DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}${SHA1HASH}"
-    else
-        DESTINATIONDIRECTORY="${REPLICABLEDIRECTORY}/${SHA1HASH}"
-    fi
-    if echo "${DEBUGDIRECTORY}" > "DESTINATION.txt"
-    then
-        export REVERTDESTINATIONINFO=1
-    fi
-    cd "${REPLICABLEEXPERIMENTDIRECTORY}"
-    REPLICABLEEXPERIMENTSHA1="$(git rev-parse --short HEAD)"
-    cd "${CURRENTDIRECTORY}"
-    chmod -R +rwx "${DESTINATIONDIRECTORY}"
-    echo "$1 ${REPLICABLEEXPERIMENTSHA1}" >> "${DESTINATIONDIRECTORY}/DIARY.txt"
-    chmod -R +rx "${DESTINATIONDIRECTORY}"
+    
+    write_to_diary
+    lock_destination_directory
     return 0
 }
